3dd6bd93600c68ea38ec9c618429251da0cd3960
mamboitaliano21 <denisthamrin@gmail.com>
2026-04-30 22:57:59 +0700

    Move audit comment after enqueue loop to avoid retry duplicates

--- app/sidekiq/refund_unpaid_purchases_worker.rb
+++ app/sidekiq/refund_unpaid_purchases_worker.rb
@@ -8,6 +8,11 @@ class RefundUnpaidPurchasesWorker
     user = User.find(user_id)
     return unless user.suspended?
 
+    unpaid_balance_ids = user.balances.unpaid.ids
+    user.sales.where(purchase_success_balance_id: unpaid_balance_ids).successful.not_fully_refunded.ids.each do |purchase_id|
+      RefundPurchaseWorker.perform_async(purchase_id, admin_user_id)
+    end
+
     admin = User.find(admin_user_id)
     user.comments.create!(
       author_id: admin.id,
@@ -15,10 +20,5 @@ class RefundUnpaidPurchasesWorker
       comment_type: Comment::COMMENT_TYPE_REFUND_BALANCE,
       content: "Refund balance initiated by #{admin.name_or_username}."
     )
-
-    unpaid_balance_ids = user.balances.unpaid.ids
-    user.sales.where(purchase_success_balance_id: unpaid_balance_ids).successful.not_fully_refunded.ids.each do |purchase_id|
-      RefundPurchaseWorker.perform_async(purchase_id, admin_user_id)
-    end
   end
 end
